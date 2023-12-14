44fbf31e9cbac0c568458135f697cabe1776aee8
Biswajit <biswajitghosh150803@gmail.com>
2023-12-14 13:11:32 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
-mongoose.connect("mongodb+srv://biswa:biswa@estate.njsibiq.mongodb.net/?retryWrites=true&w=majority").then(()=>{
-    console.log('db connected');
-}).catch((err)=>{  
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+mongoose
+  .connect(
+    "mongodb+srv://biswa:biswa@estate.njsibiq.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
     console.log(err);
+  });
+const app = express();
+app.listen(3000, () => {
+  console.log("server on port 3000");
 });
-const app=express();
-app.listen(3000,()=>{
-    console.log('server on port 3000');
-});
+
+app.use("/api/user", userRouter);
