34872f566ae3e31b716d122cbbf2f58e10c4b021
Biswajit Ghosh <biswajitghosh150803@gmail.com>
2023-12-21 02:34:33 +0530

    update listing routes

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,4 +1,4 @@
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, getLisitng, updateListing } from "../controllers/listing.controller.js";
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -6,4 +6,6 @@ const router=express.Router();
 
 router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteListing)
+router.post('/update/:id',verifyToken,updateListing)
+router.get('/get/:id',getLisitng) //anyone can view listing details so no need to verify token
 export default router;
