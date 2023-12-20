f80bb289eb4adb0239c9d204b30abd587649d2f1
Biswajit Ghosh <biswajitghosh150803@gmail.com>
2023-12-20 17:44:45 +0530

    create listing routes and links

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import {app} from '../firebase';
 import { updateUserStart,updateUserSuccess,updateUserFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure, signOutUserStart } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -150,6 +151,9 @@ const handleSignOut=async()=>{
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:op-95'>
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link to='/create-listing' className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
+          Create Listing
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
