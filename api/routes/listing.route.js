e18fe2d8ca67ac304d716347b670b56a60d55276
Biswajit Ghosh <biswajitghosh150803@gmail.com>
2023-12-20 22:18:04 +0530

    create listing api routes

---
+++ api/routes/listing.route.js
@@ -0,0 +1,8 @@
+import { createListing } from "../controllers/listing.controller.js";
+import express from "express";
+import { verifyToken } from "../utils/verifyUser.js";
+
+const router=express.Router();
+
+router.post('/create',verifyToken,createListing)
+export default router;
