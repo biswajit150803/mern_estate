3b34716e2839a8ac43b028d26e2e7dc8625c1e2a
Biswajit Ghosh <biswajitghosh150803@gmail.com>
2023-12-24 01:48:25 +0530

    Changes in Listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -83,14 +83,14 @@ export default function Listing() {
             </p>
           )}
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
-            <p className='text-2xl font-semibold'>
+            <p className='text-4xl font-semibold'>
               {listing.name} - ${' '}
               {listing.offer
                 ? listing.discountPrice.toLocaleString('en-US')
                 : listing.regularPrice.toLocaleString('en-US')}
               {listing.type === 'rent' && ' / month'}
             </p>
-            <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+            <p className='flex items-center mt-3 gap-2 text-slate-600  text-lg'>
               <FaMapMarkerAlt className='text-green-700' />
               {listing.address}
             </p>
